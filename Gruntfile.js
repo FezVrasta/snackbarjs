6cf23884a332c11bbe94f81bad46879030ddb1c3
Joonas Meriläinen <joonas.m.merilainen@metropolia.fi>
2014-09-30 17:45:19 +0300

    Grunt & task for js linting

---
+++ Gruntfile.js
@@ -0,0 +1,17 @@
+"use strict";
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+    jshint: {
+      all: ["Gruntfile.js", "src/**/*.js"]
+    }
+
+  });
+
+  grunt.loadNpmTasks("grunt-contrib-jshint");
+
+  grunt.registerTask("test", ["jshint"]);
+
+};
