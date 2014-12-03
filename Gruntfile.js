361c0e38b846f8b7c01ed6454aae762208b93747
FezVrasta <fez@tekhy.net>
2014-12-03 12:03:41 +0100

    added grunt tasks and maps

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,12 +5,66 @@ module.exports = function(grunt) {
 
     jshint: {
       all: ["Gruntfile.js", "src/**/*.js"]
+    },
+
+    less: {
+      snackbar: {
+        options: {
+          paths: ["less"],
+          sourceMap: true,
+          sourceMapRootpath: "/",
+          sourceMapFilename: "dist/snackbar.css.map",
+          sourceMapURL: "snackbar.css.map"
+        },
+        files: {
+          "dist/snackbar.css": "src/snackbar.less",
+        }
+      },
+      themes: {
+        options: {
+          paths: ["themes-less"]
+        },
+        files: [{
+          expand: true,
+          cwd: "themes-less",
+          src: "*.less",
+          dest: "themes-css",
+          ext: ".css"
+        }]
+      }
+    },
+
+    cssmin: {
+      snackbar: {
+        expand: true,
+        cwd: "dist",
+        src: ["*.css", "!*.min.css"],
+        dest: "dist",
+        ext: ".min.css"
+      }
+    },
+
+    uglify: {
+      options: {
+        sourceMap: true
+      },
+      snackbar: {
+        files: {
+          "dist/snackbar.min.js": "src/snackbar.js"
+        }
+      }
     }
 
   });
 
   grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.loadNpmTasks("grunt-contrib-copy");
+  grunt.loadNpmTasks("grunt-contrib-cssmin");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-contrib-less");
 
   grunt.registerTask("test", ["jshint"]);
+  grunt.registerTask("dist", ["less:snackbar", "cssmin:snackbar", "uglify:snackbar"]);
+  grunt.registerTask("themes", ["less:themes"]);
 
 };
