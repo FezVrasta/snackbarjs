ebeb75edfdca7476152170f7ae36cd647c248618
kevguy <kevlai22@uw.edu>
2017-02-01 15:57:34 +0800

    feat: add container option

--- src/snackbar.js
+++ src/snackbar.js
@@ -13,9 +13,7 @@
     }
 }(function( $ ){
 
-    $(document).ready(function() {
-        $("body").append("<div id=snackbar-container/>");
-    });
+    
 
     function isset(variable) {
         if (typeof variable !== "undefined" && variable !== null) {
@@ -46,63 +44,77 @@
       }
     };
 
-    $(document)
-    .on("click", "[data-toggle=snackbar]", function() {
-        $(this).snackbar("toggle");
-    })
-    .on("click", "#snackbar-container .snackbar", function() {
-        $(this).snackbar("hide");
-    });
+    
 
     $.snackbar = function(options) {
+
+
 
         if (isset(options) && options === Object(options)) {
             var $snackbar;
 
-			var snackbarNew = false;
+            var snackbarNew = false;
+
+            var container = "snackbar-container";
+
+            if (!isset(options.container)){
+                $(document).ready(function() {
+                    $("body").append("<div id=snackbar-container/>");
+                });
+            } else {
+                container = options.container;
+            }
+
+            $(document)
+            .on("click", "[data-toggle=snackbar]", function() {
+                $(this).snackbar("toggle");
+            })
+            .on("click", "#" + container + " .snackbar", function() {
+                $(this).snackbar("hide");
+            });
 
             if (!isset(options.id)) {
                 options.id = "snackbar" + Date.now();
                 $snackbar = $("<div/>").attr("id", options.id).attr("class", "snackbar");
-				snackbarNew = true;
+                snackbarNew = true;
             } else {
                 if ($("#" + options.id).length) {
-					$snackbar = $("#" + options.id);
-				} else {
-					$snackbar = $("<div/>").attr("id", "" + options.id).attr("class", "snackbar");
-					snackbarNew = true;
-				}
+                    $snackbar = $("#" + options.id);
+                } else {
+                    $snackbar = $("<div/>").attr("id", "" + options.id).attr("class", "snackbar");
+                    snackbarNew = true;
+                }
             }
 
             var snackbarStatus = $snackbar.hasClass("snackbar-opened");
 
             if (isset(options.style)) {
-				if (snackbarStatus) {
-					$snackbar.attr("class", "snackbar snackbar-opened " + options.style);
+                if (snackbarStatus) {
+                    $snackbar.attr("class", "snackbar snackbar-opened " + options.style);
                 } else {
-					$snackbar.attr("class", "snackbar " + options.style);
+                    $snackbar.attr("class", "snackbar " + options.style);
                 }
-				$snackbar.attr("data-style", options.style);
+                $snackbar.attr("data-style", options.style);
             } else {
                 if (snackbarStatus) {
-					$snackbar.attr("class", "snackbar snackbar-opened");
+                    $snackbar.attr("class", "snackbar snackbar-opened");
                 } else {
-					$snackbar.attr("class", "snackbar");
+                    $snackbar.attr("class", "snackbar");
                 }
             }
 
             options.htmlAllowed = isset(options.htmlAllowed) ? options.htmlAllowed : false;
 
             options.timeout = (isset(options.timeout)) ? options.timeout : 3000;
-			$snackbar.attr("data-timeout", options.timeout);
+            $snackbar.attr("data-timeout", options.timeout);
 
             options.content = (options.htmlAllowed) ? options.content : $("<p>" + options.content + "</p>").text();
 
             if (isset(options.onClose)) events.on(options.id, options.onClose);
 
-			if (isset(options.htmlAllowed)) {
-				$snackbar.attr("data-html-allowed", options.htmlAllowed);
-			}
+            if (isset(options.htmlAllowed)) {
+                $snackbar.attr("data-html-allowed", options.htmlAllowed);
+            }
 
             if (isset(options.content)) {
                 if ($snackbar.find(".snackbar-content").length) {
@@ -110,13 +122,13 @@
                 } else {
                     $snackbar.prepend("<span class=snackbar-content>" + options.content + "</span>");
                 }
-				$snackbar.attr("data-content", options.content);
+                $snackbar.attr("data-content", options.content);
             }
 
             if (snackbarNew) {
-                $snackbar.appendTo("#snackbar-container");
+                $snackbar.appendTo("#" + container);
             } else {
-                $snackbar.insertAfter("#snackbar-container .snackbar:last-child");
+                $snackbar.insertAfter("#" + container + " .snackbar:last-child");
             }
 
             // Show or hide item
@@ -150,6 +162,7 @@
                 setTimeout(function() {
                     if ($snackbar.data("animationId2") === animationId2) {
                         $snackbar.removeClass("snackbar-opened");
+                        $snackbar.hide();
                         events.emit(options.id);
                         events.off(options.id);
                     }
@@ -196,7 +209,7 @@
         } else {
 
             options = {
-					id: this.attr("id"),
+                    id: this.attr("id"),
                     content: $(this).attr("data-content"),
                     style: $(this).attr("data-style"),
                     timeout: parseInt($(this).attr("data-timeout")),
