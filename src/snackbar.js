4a8c119570443962519977b6357d133f682df565
John Zhang <zsgsdesign@gmail.com>
2020-05-01 17:32:40 +0800

    Update snackbar.js

--- src/snackbar.js
+++ src/snackbar.js
@@ -14,7 +14,7 @@
 }(function( $ ){
 
     $(document).ready(function() {
-        $("body").append("<div id=snackbar-container/>");
+        $("body").append("<div id=snackbar-container></div>");
     });
 
     function isset(variable) {
